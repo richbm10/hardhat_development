116304cd4137e6a781758bf33e7c775daa7968ba
Ricardo Bonilla Morales <ricardobonillamorales@Ricardos-MacBook-Pro.local>
2024-07-24 12:35:17 -0600

    Set up deployment environment variables using Alchemy node provider and private key for the signature

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { ALCHEMY_API_KEY, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    optimismSepolia: {
+      url: `https://opt-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    baseSepolia: {
+      url: `https://base-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
